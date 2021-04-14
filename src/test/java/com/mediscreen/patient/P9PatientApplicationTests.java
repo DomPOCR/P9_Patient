b6856a47f5838730d321efd0b241dcca81c2daaa
DomPOCR <dominiquep.technocenter@gmail.com>
2021-04-14 15:14:46 +0200

    Initial commit

---
+++ src/test/java/com/mediscreen/patient/P9PatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P9PatientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
