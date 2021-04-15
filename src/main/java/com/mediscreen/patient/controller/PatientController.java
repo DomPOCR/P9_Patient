649568d948a6c125c14b55de771787320b55a7fb
DomPOCR <dominiquep.technocenter@gmail.com>
2021-04-15 16:11:56 +0200

    patient controller + model + dao

---
+++ src/main/java/com/mediscreen/patient/controller/PatientController.java
@@ -0,0 +1,10 @@
+package com.mediscreen.patient.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PatientController {
+
+
+}
