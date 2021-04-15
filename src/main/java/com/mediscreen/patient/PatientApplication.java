938cac4242cb2b1de2ebeac3c1d466fbf8837071
DomPOCR <dominiquep.technocenter@gmail.com>
2021-04-15 15:07:53 +0200

    Init pom.xml

--- src/main/java/com/mediscreen/patient/PatientApplication.java
+++ src/main/java/com/mediscreen/patient/PatientApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class P9PatientApplication {
+public class PatientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(P9PatientApplication.class, args);
+        SpringApplication.run(PatientApplication.class, args);
     }
 
 }
