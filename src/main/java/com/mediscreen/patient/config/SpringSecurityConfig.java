88d3c5c86326b2a057339f7b2074152957466950
DomPOCR <dominiquep.technocenter@gmail.com>
2021-06-09 11:22:34 +0200

    Refactor

--- src/main/java/com/mediscreen/patient/config/SpringSecurityConfig.java
+++ src/main/java/com/mediscreen/patient/config/SpringSecurityConfig.java
@@ -19,8 +19,12 @@ import org.springframework.security.web.authentication.logout.SecurityContextLog
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
     private MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    public SpringSecurityConfig (MyUserDetailsService myUserDetailsService) {
+        this.myUserDetailsService=myUserDetailsService;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
